453982db1c459de90cad1ccd7639fbd65dbaf918
VieQuy <fanbox2003@gmail.com>
2023-10-09 01:12:50 +0700

    First upload - Mario style

---
+++ code/game_data.py
@@ -0,0 +1,16 @@
+level_0 = {
+		'terrain': '../levels/0/Map_terrain.csv',
+		'fg palms':'../levels/0/Map_fg_palms.csv',
+		'bg palms':'../levels/0/Map_bg_palms.csv',
+		'crates': '../levels/0/Map_crates.csv',
+		'enemies':'../levels/0/Map_enemies.csv',
+		'constraints':'../levels/0/Map_constraints.csv',
+		'player': '../levels/0/Map_player.csv',
+		'grass': '../levels/0/Map_grass.csv',
+		'node_pos': (110,400),
+		'unlock': 1,
+		'node_graphics': '../graphics/overworld/0'}
+
+levels = {
+	0: level_0,
+}
