d771dd618aca168103bc39999364b3b116c69292
VieQuy <fanbox2003@gmail.com>
2023-10-15 18:48:19 +0700

    map_updating

--- levels/tilesets/terrain_tiles.tsx
+++ levels/tilesets/terrain_tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="terrain_tiles" tilewidth="64" tileheight="64" tilecount="16" columns="4">
- <image source="../../graphics/terrain/terrain_tiles.png" width="256" height="256"/>
+<tileset version="1.10" tiledversion="1.10.2" name="terrain_tiles" tilewidth="64" tileheight="64" tilecount="16" columns="4">
+ <image source="../../Graphics/terrain/terrain_tiles.png" width="256" height="256"/>
 </tileset>
--- levels/tilesets/ufo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Ufo" tilewidth="64" tileheight="64" tilecount="1" columns="1">
- <image source="../../../../BTL/miniProjectPy-ptitCourse/graphics/enemy/plate/1.png" width="64" height="64"/>
-</tileset>
